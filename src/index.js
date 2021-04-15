9275c7d43a71805fcc3a4ab6f830c39e2558ed80
thneves <tsneves11@gmail.com>
2021-04-15 14:55:17 -0300

    Setup webpack, initial config and scripts

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+let content = document.getElementById('content');
+let h1 = document.createElement('h1');
+h1.innerHTML = 'Weather';
+h1.classList.add('color')
+content.appendChild(h1);
