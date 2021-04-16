d54a0a24c171bb21627d0b55bcff4cd1d382c03c
thneves <tsneves11@gmail.com>
2021-04-16 20:03:55 -0300

    Display current weather at page

--- src/form.js
+++ src/form.js
@@ -1,10 +1,11 @@
-import weatherInfo from './index'
-
+import weatherInfo from './index';
+import weatherInfoDiv from './index';
 var form = document.createElement("form");
 
 var input = document.createElement("input"); //input element, text
 input.setAttribute('type',"text");
 input.setAttribute('id',"city-name");
+input.setAttribute('placeholder','City')
 
 var submitBtn = document.createElement("button"); //input element, Submit button
 submitBtn.setAttribute('type',"submit");
@@ -17,6 +18,7 @@ form.appendChild(submitBtn);
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   let cityName = document.getElementById('city-name').value
+  weatherInfoDiv.innerHTML = ``
   // console.log(cityName)
   weatherInfo(cityName)
 }) 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './styles.css';
 import form from './form'
+import bgImg from './synthwave.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const myImg = new Image();
+myImg.src = bgImg;
 
 let content = document.getElementById('content');
 let h1 = document.createElement('h1');
@@ -7,17 +12,11 @@ h1.innerHTML = 'Weather App';
 h1.classList.add('color')
 content.appendChild(h1);
 content.appendChild(form);
-const weatherCityInfo = {
-  description: '',
-  wind: '',
-  humidity: '',
-  temperature: ''
-}
-
+const weatherInfoDiv = document.createElement('div');
 
 export default async function weatherInfo(city) {
 try {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=749526431275a6828d7b2107b0a88638`, {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=749526431275a6828d7b2107b0a88638&units=metric`, {
         mode: 'cors'
       })
     if(response.status == "404") {
@@ -25,15 +24,29 @@ try {
       throw "City not found";
     }
     let weatherData = await response.json();
-    weatherCityInfo.description = weatherData.weather[0].description
-    weatherCityInfo.wind = weatherData.wind.speed
-    weatherCityInfo.humidity = weatherData.main.humidity
-    weatherCityInfo.temperature = weatherData.main.temp
     console.log(weatherData)
-    console.log(weatherData.weather[0].description)
-    console.log(weatherData.wind.speed + 'mts/s vento')
-    console.log(weatherData.main.humidity + '% de humidade')
-    console.log(weatherData.main.temp + ' Cº')
+    console.log(weatherData.sys.country)
+
+    
+    weatherInfoDiv.classList.add('weatherInfoDiv')
+    weatherInfoDiv.innerHTML = `
+      <h2 id="city">${weatherData.name}, ${weatherData.sys.country}</h2>
+      <h3 id="temperature">${Math.round(weatherData.main.temp) }C°<h3>
+      <h3 id="description">${weatherData.weather[0].description}</h3>
+      <h5 id="date">Hoje</h5>
+      <div>
+        <span id="wind">Wind: ${Math.round(weatherData.wind.speed)} mts/s</span><span id="humidity">Humidity: ${weatherData.main.humidity}%</span> 
+      </div>
+    `
+    content.appendChild(weatherInfoDiv);
+
+    // console.log(weatherData.name)
+    // console.log(weatherData)
+    // console.log(weatherData.weather[0].description)
+    // console.log(weatherData.wind.speed + 'mts/s vento')
+    // console.log(weatherData.main.humidity + '% de humidade')
+    // console.log(weatherData.main.temp + ' Cº')
+    // console.log(weatherCityInfo)
   } catch (error) {
     console.log(error)
   } 
@@ -41,5 +54,5 @@ try {
 
 // weatherInfo();
 
-console.log(weatherCityInfo)
+// console.log(weatherCityInfo)
 
